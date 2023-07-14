3641ecda0f616d348feeda0e4273f58b150b0119
Julianwebdev <135730269+Julianwebdev@users.noreply.github.com>
2023-07-14 18:11:36 +0200

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+		var typed = new typed(".multiple-text", {
+		strings: ["Ready to make your website!", "Klaar om uw website te maken!", "Web Developer"]
+	typeSpeed: 200,
+	backSpeed: 200,
+	backDelay: 2000,
+	loop: true
+		})
